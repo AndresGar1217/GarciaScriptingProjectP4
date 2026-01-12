bf5c1d7e0bc5552e708798ab3187c515f6fe4eea
AndresGar1217 <731366@student.dusd.net>
2026-01-12 11:11:33 -0800

    worked on
    update and fixed updare

---
+++ P4GarciaScriptingProject/Assets/Scripts/UpdateAndFixedUpdate.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAndFixedUpdate : MonoBehaviour
+{
+    private float fixedUpdateTimer;
+    private float UpdateTimer;
+
+    // Start is called before the first frame update
+   void FixedUpdate ()
+    {
+        Debug.Log("FixedUpdate time :" + Time.deltaTime);
+    }
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update time :" + Time.deltaTime);
+    }
+}
