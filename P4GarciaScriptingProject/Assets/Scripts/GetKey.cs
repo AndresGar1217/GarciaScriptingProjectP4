e6b1511a7a58970a8cb720468bbc888c87662209
AndresGar1217 <731366@student.dusd.net>
2026-03-09 11:02:18 -0700

    finished getbutton/key

--- P4GarciaScriptingProject/Assets/Scripts/GetKey.cs
+++ P4GarciaScriptingProject/Assets/Scripts/GetKey.cs
@@ -15,7 +15,7 @@ public class GetKey : MonoBehaviour
     {
         bool down = Input.GetKeyDown(KeyCode.Space);
         bool held = Input.GetKey(KeyCode.Space);
-        bool up = Input GetKeyUp(KeyCode.Space);
+        bool up = Input.GetKeyUp(KeyCode.Space);
 
         if (down)
         {
@@ -23,7 +23,17 @@ public class GetKey : MonoBehaviour
         }
         else if (held)
         {
-            Debug.Log("Key hells is: " + down);
+            Debug.Log("Key held is: " + held);
+        }
+        else if (up)
+        {
+            Debug.Log("Get Key Up is: " + up);
+        }
+        else
+        {
+            Debug.Log("Get Key Down is: " + down);
+            Debug.Log("Key held is: " + held);
+            Debug.Log("Get Key Up is: " + up);
         }
       
     }
